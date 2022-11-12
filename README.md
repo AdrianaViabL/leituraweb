926b28c5d922f982ff839ae961a16f977c0f784b
adriana <adrianaviabl@yahoo.com.br>
2022-11-12 19:17:29 -0300

    new info

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # leituraweb
 atividade extra
+
+ativando ambiente virtual
+``` bash
+source venv/local/bin/activate
+```
--- main.py
+++ main.py
@@ -1,16 +1,23 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+from bs4 import BeautifulSoup
+import requests
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def web_reader(link_list):
+    request = {}
+    for page in link_list:
+        html_page = requests.get(page)
+        if html_page.status_code != 200:
+            break
+        soup = BeautifulSoup(html_page.content, 'html.parser')
+
+        warning = soup.findAll('img')
+        book_container = warning.nextSibling.nextSibling
+        print(book_container)
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    link_list = ['http://books.toscrape.com/']
+    web_reader(link_list)
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
