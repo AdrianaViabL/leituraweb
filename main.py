222dfd9e3895e8cc002fd8f2ae028eb46cfed7cb
adriana <adrianaviabl@yahoo.com.br>
2022-11-13 21:46:05 -0300

    new structure, get png link, phone and send back the json

--- main.py
+++ main.py
@@ -1,23 +1,34 @@
+import json
+import re
+
 from bs4 import BeautifulSoup
 import requests
 
 
 def web_reader(link_list):
-    request = {}
+    request = []
+    logo_url = []
+    reg = r'((?:\+\d{2}[-\.\s]??|\d{4}[-\.\s]??)?(?:\d{3}[-\.\s]??\d{3}[-\.\s]??\d{4}|\(\d{3}\)\s*\d{3}[-\.\s]??\d{4}|\d{3}[-\.\s]??\d{4}))'
     for page in link_list:
         html_page = requests.get(page)
         if html_page.status_code != 200:
+
             break
         soup = BeautifulSoup(html_page.content, 'html.parser')
+        getStuff = soup.findAll('img', {'src': re.compile('png')})
+        phone = soup.findAll('text', re.compile(reg))
+        for stuff in getStuff:
+            logo_url.append(f"{page}{stuff['src']}")
+        result = dict({
+            'logo': logo_url,
+            'phone': phone,
+            'website': page})
+        request.append(result)
+        print(getStuff)
+    return json.dumps(request)
 
-        warning = soup.findAll('img')
-        book_container = warning.nextSibling.nextSibling
-        print(book_container)
 
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    link_list = ['http://books.toscrape.com/']
+    link_list = ['https://pt.wikipedia.org/wiki/Portal:Hist%C3%B3ria',
+                 'https://stackoverflow.com/questions/3868753/find-usa-phone-numbers-in-python-script/28864077#28864077']
     web_reader(link_list)
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
