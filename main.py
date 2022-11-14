7169461e5313a91c28eaf102973bd501782e71c9
adriana <adrianaviabl@yahoo.com.br>
2022-11-13 22:51:41 -0300

    adjustings

--- main.py
+++ main.py
@@ -8,27 +8,29 @@ import requests
 def web_reader(link_list):
     request = []
     logo_url = []
-    reg = r'((?:\+\d{2}[-\.\s]??|\d{4}[-\.\s]??)?(?:\d{3}[-\.\s]??\d{3}[-\.\s]??\d{4}|\(\d{3}\)\s*\d{3}[-\.\s]??\d{4}|\d{3}[-\.\s]??\d{4}))'
+    reg = r"(\(?\d{2}\)?\s)?(\d{4,5}\-\d{4})"
     for page in link_list:
-        html_page = requests.get(page)
-        if html_page.status_code != 200:
-
-            break
-        soup = BeautifulSoup(html_page.content, 'html.parser')
-        getStuff = soup.findAll('img', {'src': re.compile('png')})
-        phone = soup.findAll('text', re.compile(reg))
-        for stuff in getStuff:
-            logo_url.append(f"{page}{stuff['src']}")
-        result = dict({
-            'logo': logo_url,
-            'phone': phone,
-            'website': page})
-        request.append(result)
-        print(getStuff)
+        try:
+            html_page = requests.get(page)
+            soup = BeautifulSoup(html_page.content, 'html.parser')
+            getStuff = soup.findAll('img', {'src': re.compile('png')})
+            phone = re.findall(reg, soup.text)
+            for stuff in getStuff:
+                if 'logo' in stuff['src']:
+                    if 'http' in stuff['src']:
+                        logo_url.append(stuff['src'])
+                    else:
+                        logo_url.append(f"{page}{stuff['src']}")
+            result = dict({
+                'logo': logo_url,
+                'phone': phone,
+                'website': page})
+            request.append([result])
+        except Exception as ex:
+            request.append({'erro_message': f'erro no processo {ex}'})
     return json.dumps(request)
 
 
 if __name__ == '__main__':
-    link_list = ['https://pt.wikipedia.org/wiki/Portal:Hist%C3%B3ria',
-                 'https://stackoverflow.com/questions/3868753/find-usa-phone-numbers-in-python-script/28864077#28864077']
+    link_list = []
     web_reader(link_list)
